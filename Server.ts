9d2f99b2c9513efe15f74df8d9824f497d7d0569
OlmoDalco <yannik.hake@gmail.com>
2021-02-11 15:28:28 +0100

    Update Server.ts

--- Server.ts
+++ Server.ts
@@ -1,12 +1,15 @@
 import { serve} from "https://deno.land/std@0.86.0/http/server.ts";
 import { Status } from "https://deno.land/std@0.53.0/http/http_status.ts";
-
+import {Person} from "./Person.ts"
 class Service {
+    Person:Person
     constructor() {
         this.start()
+        this.Person = new Person()
     }
     private async start() {
         let s = serve({ port: 8000 })
+        console.log(this.Person.name)
         for await (const req of s) {
             const headers = new Headers();
             headers.append("Content-Type", "application/json")
